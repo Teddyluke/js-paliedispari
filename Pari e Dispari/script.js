8d45c3088e6106bcba0ffad65653b0235f6a2260
Luca Maglie <luca.maglie2@gmail.com>
2020-06-29 15:53:37 +0200

    pari e dispari completamento

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -3,10 +3,40 @@ var scelta = prompt("pari o dispari?");
 
 var numUtente = parseInt(prompt("inserisci un numero."));
 
-var numPc = Math.floor(Math.random() * 5);
+var numPc = getRandomInt(1, 5);
+
 
 console.log(numPc);
 
-var somma = numUtente + numpc;
+var somma = numUtente + numPc;
 
 console.log(somma);
+
+if (isEven(somma) == true && scelta == "pari") {
+  console.log(" hai vinto");
+}
+else if (isEven(somma) == false && scelta == "dispari") {
+  console.log(" hai vinto");
+
+} else {
+  console.log( "hai perso");
+}
+
+
+// funzione che ritorna un numero intero randomico tra un min e un max range
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// funziona che ritorna se un numero è pari o dispari
+function isEven(num) {
+  if (num % 2 == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
