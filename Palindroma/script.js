3ef3845de2c3595b74279ae33a28bf274b9e9909
Luca Maglie <luca.maglie2@gmail.com>
2020-06-29 15:33:43 +0200

    invio palindromo con soluzione trovata in rete.

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,37 +1,76 @@
 // chiedo all'utente di inserire una parola
 var nome = prompt("inserisci un nome.");
 
-
-var nomeDritto = [];
-var nomeReverse = [];
-// tratto la parola come un array e cerco di invertirne l'ordine
-for (var i = 0; i < nome.length; i++) {
-  nomeReverse.push(nome[i]);
-  nomeDritto.push(nome[i]);
-}
-nomeReverse.reverse();
+//  uso la funzione per invertire il nome
+var nomeReverse = reverseString(nome);
+console.log(nome);
 console.log(nomeReverse);
-console.log(nomeDritto);
 
-// confronto le due stringhe per verificare che siano identiche
+//  confronto le due stringhe.
+if (nome == nomeReverse) {
+  console.log("il nome è palindromo");
 
-
-var i = 0
-var confronto = false;
-while ( i < nomeDritto.length ) {
-  if (nomeDritto[i] == nomeReverse[i] ) {
-    confronto = true;
-  }
-  else {
-    confronto = false;
-  }
-  i++;
-  console.log(confronto);
+}else {
+  console.log("il nome non è palindromo");
 }
 
-if (confronto = true) {
-  console.log(" i nomi sono palindromi");
+
+
+
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
 }
-else {
-  console.log("i nomi non sono palindromi");
-}
+
+
+
+
+
+
+
+
+// var nomeDritto = [];
+// var nomeReverse = [];
+// // tratto la parola come un array e cerco di invertirne l'ordine
+// for (var i = 0; i < nome.length; i++) {
+//   nomeReverse.push(nome[i]);
+//   nomeDritto.push(nome[i]);
+// }
+// nomeReverse.reverse();
+// console.log(nomeReverse);
+// console.log(nomeDritto);
+//
+// // confronto le due stringhe per verificare che siano identiche
+//
+//
+// var i = 0
+// var confronto = false;
+// while ( i < nomeDritto.length ) {
+//   if (nomeDritto[i] == nomeReverse[i] ) {
+//     confronto = true;
+//   }
+//   else {
+//     confronto = false;
+//   }
+//   i++;
+//   console.log(confronto);
+// }
+//
+// if (confronto = true) {
+//   console.log(" i nomi sono palindromi");
+// }
+// else {
+//   console.log("i nomi non sono palindromi");
+// }
---
+++ Pari e Dispari/script.js
@@ -0,0 +1,12 @@
+
+var scelta = prompt("pari o dispari?");
+
+var numUtente = parseInt(prompt("inserisci un numero."));
+
+var numPc = Math.floor(Math.random() * 5);
+
+console.log(numPc);
+
+var somma = numUtente + numpc;
+
+console.log(somma);
